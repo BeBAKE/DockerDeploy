1a3670b35202666e4ab6aade0acbfe3a40a7d961
Vivek <viveksinghcharak004@gmail.com>
2025-04-19 19:48:15 +0530

    afterSSLnginx is only for copying

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ echo "starting docker compose up"
 sudo docker compose up -d
 
 echo "copying files"
-cat ./nginx/afterSSLNginx.txt > ./nginx/nginx.conf
+cat ./nginx/afterSSLnginx.txt > ./nginx/nginx.conf
 
 echo "reloading nginx"
 sudo docker compose exec nginx nginx -s reload
