15c1479b189b725f4332806b87b4dca0d0609a87
Vivek <viveksinghcharak004@gmail.com>
2025-04-20 14:38:16 +0530

    only checking nginx before certbot

--- init.sh
+++ init.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 echo "starting docker compose up"
-sudo docker compose up -d
+sudo docker compose up -d nginx backend
 
-echo "copying files"
-cat ./nginx/afterSSLnginx.txt > ./nginx/nginx.conf
-
-# Wait for nginx container to be up and healthy
 echo "Waiting for nginx to be healthy..."
 until sudo docker compose exec nginx nginx -t &>/dev/null; do
   sleep 1
 done
+
+echo "starting certbot now"
+docker compose up certbot
+
+echo "copying files"
+cat ./nginx/afterSSLnginx.txt > ./nginx/nginx.conf
 
 echo "nginx is ready, reloading..."
 sudo docker compose exec nginx nginx -s reload
---
+++ restart.sh
@@ -0,0 +1,28 @@
+sudo docker compose down -v
+sudo docker rm $(sudo docker stop $(sudo docker ps -aq ))
+sudo docker rmi $(sudo docker images -q)
+
+sudo rm -rf cerbot
+
+echo "events {
+    worker_connections 1024;
+}
+
+http {
+    server_tokens off;
+    charset utf-8;c
+
+    server {
+        listen 80 ;
+        server_name test-api.projectlive.me;
+
+        location / {
+            proxy_pass http://backend:5500/;
+        }
+
+        location /.well-known/acme-challenge/ {
+            root /var/www/certbot;
+        }
+    }
+
+}" > nginx/nginx.conf
