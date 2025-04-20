3a260dc95c51fc4de5db11066585a8dac2c074b6
Vivek <viveksinghcharak004@gmail.com>
2025-04-20 13:32:54 +0530

    nginx container takes time to fully laod up

--- init.sh
+++ init.sh
@@ -6,5 +6,11 @@ sudo docker compose up -d
 echo "copying files"
 cat ./nginx/afterSSLnginx.txt > ./nginx/nginx.conf
 
-echo "reloading nginx"
+# Wait for nginx container to be up and healthy
+echo "Waiting for nginx to be healthy..."
+until sudo docker compose exec nginx nginx -t &>/dev/null; do
+  sleep 1
+done
+
+echo "nginx is ready, reloading..."
 sudo docker compose exec nginx nginx -s reload
