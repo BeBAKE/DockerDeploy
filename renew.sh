0bdb83d1eb2cba4c9ac6b9d5fc9582e7c24d5a81
Vivek <viveksinghcharak004@gmail.com>
2025-04-20 16:07:25 +0530

    script for cert renewal

--- renew.sh
+++ renew.sh
@@ -1,3 +1,3 @@
-/usr/bin/docker run --rm --name viv -v "/home/ubuntu/DockerDeploy/certbot/conf:/etc/letsencrypt" certbot/certbot certificates 2>&1 | /usr/bin/tee -a /home/ubuntu/DockerDeploy/one.txt
+/usr/bin/docker run --rm --name viv -v "/home/ubuntu/DockerDeploy/certbot/conf:/etc/letsencrypt" certbot/certbot renew 2>&1 | /usr/bin/tee -a /home/ubuntu/DockerDeploy/one.txt
 
 # * * * * * /home/ubuntu/DockerDeploy/renew.sh >> /home/ubuntu/DockerDeploy/cron.log 2>&1
